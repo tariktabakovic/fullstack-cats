06da125ea6d42850b7091e438d39c0e3dee37f7a
Tarik Tabakovic <tariktabakovic@Tariks-MacBook-Air.local>
2020-01-13 10:05:14 -0500

    adds sql files

---
+++ schema.sql
@@ -0,0 +1,32 @@
+-- what do we want to know about owners?
+create table owners {
+    id serial primary key,
+    name text,
+    phone_number varchar(20)
+};
+
+-- what do we want to know about pets?
+create table pets {
+    id serial primary key,
+    name text, 
+    -- if you wanted to limit the number of characters use varchar(10) which stands
+    -- for 'character varying'
+    species varchar(100),
+    -- instead of age, we can derive the age from a birthdate
+    birthdate date,
+    -- when you have the info in another table, ask yourself, do pets have one owners?
+    -- or do pets have many owners?
+    -- if pets have one and only one, then you put the foreign key right HERE. 
+    owner_id integer references owners (id)
+    -- the integer references the id in the table 'owners'.
+};
+
+
+-- this will demo many-to-many
+-- from many-to-many, use a 'linking' table.
+
+-- create table owners_pets(
+--     -- this table needs no id 
+--     owner_id integer references owners(id),
+--     pet_id integer references pets(id)
+-- );
---
+++ seed.sql
@@ -0,0 +1,14 @@
+insert into owners
+    (name, phonenumber)
+values
+    ('Chris', '678-200-3000'),
+    ('Aylor', '404-234-0101');
+
+insert into pets
+    (name, species, birthdate, owner_id)
+values
+    ('oakley', 'cat', '2015-05-23', 1),
+    ('milla', 'tortuise', '2013-03-05', 1),
+    ('dexter', 'dog', '2012-04-14', 2),
+    ('hank', 'dog', '1999-02-18', 2),
+    ('seymour', 'cat', '2008-12-09', 2);
